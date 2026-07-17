95f1acf286b70279f5e61d12dee5584ed779553f
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-07-16 20:49:14 -0700

    Initial commit

---
+++ flattenCurve.js
@@ -0,0 +1,30 @@
+function flatten(...args) {
+  const recursiveCall = (evaluatedArray, index = 0, newArray = []) => {
+    if (index === evaluatedArray.length) {
+      return;
+    }
+
+    if (!Array.isArray(evaluatedArray[index])) {
+      newArray.push(evaluatedArray[index]);
+    }
+
+    if (Array.isArray(evaluatedArray[index])) {
+      recursiveCall(evaluatedArray[index], 0, newArray);
+    }
+
+    recursiveCall(evaluatedArray, index + 1, newArray);
+
+    return newArray;
+  };
+
+  return recursiveCall(args);
+}
+
+console.log(flatten(1, [2, 3], 4, 5, [6, [7]]));
+console.log(flatten("a", ["b", 2], 3, 666, [[4], ["c"]]));
+console.log(
+  flatten(
+    [[[[[[1]]]]], ["a", [3, ["b"]]], null, 5, "c"],
+    [1, "a", 3, "b", null, 5, "c"],
+  ),
+);
