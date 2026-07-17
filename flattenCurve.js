bba4a012d9487be9a1218374171ad53f298d8af2
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-07-17 16:48:23 -0700

    added comments

--- flattenCurve.js
+++ flattenCurve.js
@@ -1,17 +1,21 @@
 function flatten(...args) {
   const recursiveCall = (evaluatedArray, index = 0, newArray = []) => {
+    // Base case
     if (index === evaluatedArray.length) {
       return;
     }
 
+    // If element is not an array, add to newArray
     if (!Array.isArray(evaluatedArray[index])) {
       newArray.push(evaluatedArray[index]);
     }
 
+    // If element is a nested array, explore the nested array
     if (Array.isArray(evaluatedArray[index])) {
       recursiveCall(evaluatedArray[index], 0, newArray);
     }
 
+    // Increment index to explore the highest level of the array
     recursiveCall(evaluatedArray, index + 1, newArray);
 
     return newArray;
